1468cf145ecc088fb212c8ee25bc3202b46d5b6f
JaeCheon <studyJcHub@gmail.com>
2022-02-15 14:45:28 +0900

    확인 버튼 눌리면 입력한 문구를 토스트로 출력  - InputMessage 변수 생성  - edtMessage 에 적힌 문구를 String 으로 변형하여 대입  - 토스트로 변수에 저장 된 문구 출력

--- app/src/main/java/com/jc/kotlinuipractice_20220215/MainActivity.kt
+++ app/src/main/java/com/jc/kotlinuipractice_20220215/MainActivity.kt
@@ -2,10 +2,24 @@ package com.jc.kotlinuipractice_20220215
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        버튼이 눌릴때 -> 입렵된 메세지가 어떤건지 추출. -> 토스트로 띄워주기
+        btnOk.setOnClickListener {
+//            버튼이 눌릴 때마다 실행
+//             -> 입력된 메세지는? 변수에 담아두자.
+
+            val inputMessage = edtMessage.text.toString()   // EditText 의 메시지의 문구 (text)를 추출 -> String 으로 변환까지
+
+            Toast.makeText(this, inputMessage, Toast.LENGTH_SHORT).show()
+
+
+        }
     }
 }
